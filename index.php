9ce4087511ece6a945fe2a2efea6e6a1527d093a
EmilKlapija1 <emil.klapija@gmal.com>
2014-08-22 16:17:20 +0200

    added style

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
 </head>
 <body>
 
-
-<form action='process.php' method="post">
-	
-		Username: <input type="email" name="username" value="">
-		Password: <input type="password" name="password" value="">
-		<input type="submit" value="Sign In">
-        
-</form>
+<div>
+	<h5>Testna Login Forma</h5>
+	<form action='process.php' method="post">
+		
+			Username: <input type="email" name="username" value="">
+			Password: <input type="password" name="password" value="">
+			<input type="submit" value="Sign In">
+	        
+	</form>
+</div>
 
 
 
